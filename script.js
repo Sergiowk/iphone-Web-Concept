fcfe2d820216128bd8b6c74176103d1eee8132c6
Sergiowk <sergiowk94@gmail.com>
2022-01-26 00:02:59 +0100

    Content, css and js started

---
+++ script.js
@@ -0,0 +1,9 @@
+let controller = new ScrollMagic.Controller();
+//timeline for running different animations at the same time
+let timeline = new TimelineMax();
+
+/* 
+timeline
+.to('.iphone13.png', 3, {y:-600})
+.fromTo(".text", 5, { opacity:0}, { opacity:1, duration: 3});
+*/
